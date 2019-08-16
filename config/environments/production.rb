e2d1384b87a9f8ef7f8de7d6e230bd161804d286
tarosuke <felis.silvestris.catus.7777@gmail.com>
2019-08-16 17:46:54 +0900

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'still-island-72469.herokuapp.com'
   config.action_mailer.default_url_options = { host: sendgrid-globular-48492 }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
