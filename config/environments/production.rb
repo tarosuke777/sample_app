bbf7a21594fce05de99f5be9c361bf412166a153
tarosuke <felis.silvestris.catus.7777@gmail.com>
2019-08-16 17:48:05 +0900

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'still-island-72469.herokuapp.com'
-  config.action_mailer.default_url_options = { host: sendgrid-globular-48492 }
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
